78e286387fe529efa87fa78662d820d470f29126
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-08-24 20:26:29 +0530

    Revert "Removed the usage of deprecated class ViewModelProviders"
    This reverts commit 43bd865792ef3d6aeb75a1aee7828eeaef598172.

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -27,8 +27,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-//import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.example.android.eggtimernotifications.R
 import com.example.android.eggtimernotifications.databinding.FragmentEggTimerBinding
 import com.google.firebase.messaging.FirebaseMessaging
@@ -46,9 +45,7 @@ class EggTimerFragment : Fragment() {
             inflater, R.layout.fragment_egg_timer, container, false
         )
 
-        //The ViewModelProviders (plural) is deprecated.
-        //val viewModel = ViewModelProviders.of(this).get(EggTimerViewModel::class.java)
-        val viewModel = ViewModelProvider(this).get(EggTimerViewModel::class.java)
+        val viewModel = ViewModelProviders.of(this).get(EggTimerViewModel::class.java)
 
         binding.eggTimerViewModel = viewModel
         binding.lifecycleOwner = this.viewLifecycleOwner
